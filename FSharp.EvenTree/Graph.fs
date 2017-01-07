51be33287e4f09e93a115a60eeb806181a074ca8
dmitrynovik <cryinstone@gmail.com>
2017-01-07 12:51:15 +1100

    Node with edges

---
+++ FSharp.EvenTree/Graph.fs
@@ -0,0 +1,23 @@
+﻿namespace FSharp.EvenTree
+
+[<CustomEquality; NoComparison>]
+type Node = { id: int; edges: List<Node> } with
+
+    override this.GetHashCode() = this.id
+
+    override x.Equals(obj) =
+        match obj with
+            | :? Node as y -> x.id = y.id
+            | _ -> false
+
+    member this.AddEdge(n: Node): Node =
+        if List.exists (fun n2 -> n2 = n) this.edges then this
+        else { id = this.id; edges = n :: this.edges }
+
+    member this.RemoveEdge(n: Node): Node = 
+        { id = this.id; edges = List.filter (fun n2 -> n2.id <> n.id) this.edges }        
+
+
+type Forest = { roots: List<Node> }
+
+
--- FSharp.EvenTree/Library1.fs
+++
@@ -1,4 +0,0 @@
-﻿namespace FSharp.EvenTree
-
-type Class1() = 
-    member this.X = "F#"

728d819bb68542bc7153afaa038c9c27f6342fd8
dmitrynovik <cryinstone@gmail.com>
2017-01-07 17:43:59 +1100

    naming

--- FSharp.EvenTree/Graph.fs
+++ FSharp.EvenTree/Graph.fs
@@ -1,4 +1,4 @@
-﻿namespace FSharp.EvenTree
+﻿module FSharp.EvenTree
 
 type Node = int
     
@@ -20,51 +20,50 @@ type Graph(nodes: List<Node>, edges: List<Edge>) =
     static member ParseNode(sNode: string) = System.Int32.Parse sNode
 
     member this.Nodes = nodes
-    member this.NodeCount = nodes.Length
     member this.Edges = edges
 
-    member this.AddNode(n: Node): Graph =
+    member this.addNode(n: Node): Graph =
         if List.contains n nodes then this
         else Graph(n :: nodes, edges)
 
-    member this.AddEdge(e: Edge): Graph =
+    member this.addEdge(e: Edge): Graph =
         if e.v1 = e.v2 then this // ignore malformed edges like an edge to itself
         elif List.contains e edges then this
-        else Graph(this.AddNode(e.v1).AddNode(e.v2).Nodes, e :: edges)
+        else Graph(this.addNode(e.v1).addNode(e.v2).Nodes, e :: edges)
 
-    member this.RemoveEdge(e: Edge): Graph = Graph(nodes, List.except [e] edges)
+    member this.removeEdge(e: Edge): Graph = Graph(nodes, List.except [e] edges)
 
-    member this.Union(other: Graph):Graph = 
+    member this.union(other: Graph):Graph = 
         let nodes = Seq.append nodes other.Nodes |> Seq.distinct |> Seq.toList
         let edges = Seq.append edges other.Edges |> Seq.distinct |> Seq.toList
         Graph(nodes, edges)
 
-    member this.NeigborsOf(n: Node) = 
+    member this.neighborsOf(n: Node) = 
         let nEdges = List.filter (fun e -> e.v1 = n || e.v2 = n) edges
         seq { for e in nEdges do if e.v1 = n then yield e.v2 else yield e.v1 }
         
-    member this.SubTree(root: Node):Graph = 
-        let neighbors = this.NeigborsOf root
+    member this.subTree(root: Node):Graph = 
+        let neighbors = this.neighborsOf root
         let mutable g = Graph(List.Empty, List.Empty)
         if Seq.isEmpty neighbors then g
         else 
             for n in neighbors do
-                let nTree = this.SubTree n
-                g <- g.Union(nTree).AddEdge({v1 = root; v2 = n})        
+                let nTree = this.subTree n
+                g <- g.union(nTree).addEdge({v1 = root; v2 = n})        
             g
 
     member this.maxEvenDepth() =
         let root = List.min nodes
-        let neighbors = this.NeigborsOf root
+        let neighbors = this.neighborsOf root
         let mutable count = 0
         for n in neighbors do
-            let subTree = this.SubTree n
-            if subTree.NodeCount % 2 = 0 then count <- count + subTree.maxEvenDepth() + 1
+            let subTree = this.subTree n
+            if subTree.Nodes.Length % 2 = 0 then count <- count + subTree.maxEvenDepth() + 1
         count
 
-    member this.parseFromStdin() = this.parse System.Console.ReadLine
+    static member parseFromStdin():Graph = Graph.parse System.Console.ReadLine
 
-    member this.parse(readLineFunc: unit -> string):Graph = 
+    static member parse(readLineFunc: unit -> string):Graph = 
 
         let mutable graph = Graph(List.Empty, List.Empty)
 
@@ -79,8 +78,15 @@ type Graph(nodes: List<Node>, edges: List<Edge>) =
 
         for line in Seq.skip 1 lines do // skip 1st line as irrelevant
             let nodes = (line.Split ' ') |> Seq.map Graph.ParseNode |> Seq.toArray
-            graph <- graph.AddEdge { v1 = nodes.[0]; v2 = nodes.[1] }
+            graph <- graph.addEdge { v1 = nodes.[0]; v2 = nodes.[1] }
             
         graph
 
+[<EntryPoint>]
+let main args = 
+    let graph = Graph.parseFromStdin()
+    let depth = graph.maxEvenDepth()
+    System.Console.WriteLine depth
+    //System.Console.Read() |> ignore
+    0
 
